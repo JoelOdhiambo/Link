b9173f18d302ecb2f882b992c7318033df3b4c05
Ikky <ikramikky4462@gmail.com>
2020-06-02 09:52:21 +0300

    Fixed dialog error.

--- app/src/main/java/com/wesley/link/MainActivity.java
+++ app/src/main/java/com/wesley/link/MainActivity.java
@@ -8,20 +8,14 @@ import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -32,23 +26,25 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.Locale;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity implements  EmergencyContactDialog.EmergencyContactDialogListener{
-private static final int REQUEST_CALL=1;
-EditText contactInput;
-private TextView tnumber;
-private  static final String EXTRA_MESSAGE="com.wesley.link.extra.MESSAGE";
-private String profileMessage="Profile edit";
-String contact;
+public class MainActivity extends AppCompatActivity implements EmergencyContactDialog.EmergencyContactDialogListener {
+    private static final int REQUEST_CALL = 1;
+    EditText contactInput;
+    private TextView tnumber;
+    private static final String EXTRA_MESSAGE = "com.wesley.link.extra.MESSAGE";
+    private String profileMessage = "Profile edit";
+    String contact;
+    private String numberFromDialog;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==REQUEST_CALL){
-            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            }else{
-                Toast.makeText(this,"Permission DENIED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -58,7 +54,7 @@ String contact;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final MediaPlayer sirenSound=MediaPlayer.create(this,R.raw.salamisound);
+        final MediaPlayer sirenSound = MediaPlayer.create(this, R.raw.salamisound);
 //        Button edit=findViewById(R.id.button_edit);
 //
 //            edit.setOnClickListener(new View.OnClickListener() {
@@ -74,33 +70,31 @@ String contact;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                contactInput=(EditText) findViewById(R.id.emergency_number);
-                //contactInput.setText(number);
-                contactInput.getText().toString();
-                Intent i = new Intent(Intent.ACTION_CALL);
-                 String p = "tel:" + contactInput.getText().toString();
-                i.setData(Uri.parse("p"));
-
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-                }else {
-                startActivity(i);
+                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numberFromDialog));
+                if (numberFromDialog != null ){
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+                    } else {
+                        startActivity(i);
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Please enter a phone number", Toast.LENGTH_SHORT).show();
                 }
             }
 
         });
-        FloatingActionButton ring=findViewById(R.id.emergency_ring);
+        FloatingActionButton ring = findViewById(R.id.emergency_ring);
         ring.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               sirenSound.start();
+                sirenSound.start();
             }
         });
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -116,8 +110,8 @@ String contact;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater =getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -125,12 +119,12 @@ String contact;
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings:
-                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.about:
                 onClickShowAbout(R.id.about);
                 return true;
-            case  R.id.emergency_contact:
+            case R.id.emergency_contact:
                 openDialog(R.id.emergency_contact);
                 return true;
             default:
@@ -138,7 +132,8 @@ String contact;
         }
 
     }
-    public boolean onClickShowAbout(int item){
+
+    public boolean onClickShowAbout(int item) {
         AlertDialog.Builder myAboutBuilder = new AlertDialog.Builder(MainActivity.this);
         myAboutBuilder.setTitle("About App");
         myAboutBuilder.setMessage("App by Wesley Joel Odhiambo");
@@ -146,15 +141,33 @@ String contact;
         myAboutBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(),"WJ",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "WJ", Toast.LENGTH_SHORT).show();
             }
         });
         myAboutBuilder.show();
-        return  true;
+        return true;
     }
-    public boolean openDialog(int item){
-        EmergencyContactDialog contact= new EmergencyContactDialog();
-        contact.show(getSupportFragmentManager(),"Emergency Contact");
+
+    public boolean openDialog(int item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.e_contact_dialog, null);
+        final EditText dialogEditTextContact = view.findViewById(R.id.emergency_number);
+        builder.setView(view).setTitle("Emergency Contact");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                numberFromDialog = dialogEditTextContact.getText().toString();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
         return true;
     }
 
